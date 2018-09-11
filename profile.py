5a121eefbd6344f1138dbe9b1f4648897a70a6b4
Chanwoo Park <parkchan@brandeis.edu>
2018-09-11 09:05:10 -0400

    Revised Personal Web

---
+++ profile.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+@app.route("/home")
+def display_home():
+    return render_template('home.html', the_title="Home | Chanwoo")
+
+@app.route("/about-me")
+def display_aboutMe():
+	return render_template('about-me.html', the_title="About Me | Chanwoo")
+
+@app.route("/projects")
+def display_projects():
+	return render_template('projects.html', the_title="Projects | Chanwoo")
+
+@app.route("/test")
+def display_test():
+	return render_template('test.html', the_title="Test | Chanwoo")
+
+if __name__ == '__main__':
+	app.run(debug=True)
